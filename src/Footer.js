9adc02b7337356b74e67b3834cf9398eabbe65b0
Khoa88Tran <khoa88tran@gmail.com>
2023-07-20 23:26:07 -0400

    update src Jul 20 2023

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Logowhite from "./assets/UXUI/little-lemon-kit/Logo2-white.svg";
+import "./styles/Footer.css";
+
 
 const FooterList = () => {
     const items = [
@@ -7,14 +10,15 @@ const FooterList = () => {
         {text: "menu", url: "/menu"},
         {text: "reservations", url: "/home"},
         {text: "online order", url: "/online-order"},
+        {text: "log in", url: "/log-in"},
     ]
     return items;
 }
 const Footer = () => {
     return (
-        <>
-            <nav>
-                <img src="UXUI/little-lemon-kit/Logo2.svg" alt="Little Lemon logo" />
+        <div className="footer">
+            <img src={Logowhite} alt="Little Lemon logo" className="footer-logo" />
+            <nav className="footer-nav">
                 <ul>
                     {FooterList().map((item, index) => (
                         <li key={index}>
@@ -24,14 +28,14 @@ const Footer = () => {
                 </ul>
             </nav>
 
-            <div>
+            <div className="footer-contact">
                 <p><strong>Contact</strong></p>
-                <p>1234 N Harrison Street, Atlanta, USA 30033 </p>
-                <p>404-987-6543 </p>
-                <p>littlelemonrestaurant@info.com </p>
+                <p id="contact">1234 N Harrison Street, Atlanta, USA 30033 </p>
+                <p id="contact">404-987-6543 </p>
+                <p id="contact">littlelemonrestaurant@info.com </p>
             </div>
 
-            <div>
+            <div className="footer-social-medias">
                 <p><strong>Social Medias</strong></p>
                 <img />
                 <img />
@@ -39,7 +43,7 @@ const Footer = () => {
                 <img />
             </div>
 
-        </>
+        </div>
     )
 }
 
